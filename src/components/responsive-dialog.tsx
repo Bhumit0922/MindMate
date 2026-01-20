327e0c465573b9589449defd1d9e2f19673159fe
Bhumit0922 <bhumitcollage2322@gmail.com>
2026-01-20 12:22:22 +0530

    feat: implement responsive dialog for command input, enhance dashboard command component

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerDescription,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+interface ResponsiveDialogProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: (opne: boolean) => void;
+}
+
+export const ResponsiveDialog = ({
+  title,
+  description,
+  children,
+  open,
+  onOpenChange,
+}: ResponsiveDialogProps) => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 ">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,4 +1,9 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import {
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandResponsiveDialog,
+} from "@/components/ui/command";
 import { Dispatch, SetStateAction } from "react";
 
 interface Props {
@@ -9,16 +14,12 @@ interface Props {
 export const DashboardCommand = ({ open, setOpen }: Props) => {
   return (
     <>
-      <CommandDialog open= {open} onOpenChange={setOpen}>
-        <CommandInput 
-        placeholder="Find a meeting or agent"
-        />
+      <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Find a meeting or agent" />
         <CommandList>
-            <CommandItem>
-                Test
-            </CommandItem>
+          <CommandItem>Test</CommandItem>
         </CommandList>
-      </CommandDialog>
+      </CommandResponsiveDialog>
     </>
   );
 };
